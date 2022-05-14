17bbf2eb663d671ea90f25d7347a27a4a3f241e5
Astro-botto <103898237+Astro-botto@users.noreply.github.com>
2022-05-14 13:05:26 +0100

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -27,8 +27,8 @@ export default class Command extends BaseCommand {
     ];
 
     const buttonMessage: any = {
-      contentText: `_*🎊HELLO THERE BEYOND BOT HERE🎊*_`,
-      footerText: "🎇 Beyond 🎇",
+      contentText: `_*🎊HELLO THERE ASTRO BOT HERE🎊*_`,
+      footerText: "🎇 ASTRO.INC 🎇",
       buttons: buttons,
       headerType: 1,
     };
