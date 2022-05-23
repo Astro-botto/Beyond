e47d126649a8efb02d44368fa334e8dadab71df8
Astro-botto <103898237+Astro-botto@users.noreply.github.com>
2022-05-23 22:24:33 +0100

    Add files via upload

---
+++ src/commands/Moderation/support.ts
@@ -0,0 +1,30 @@
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "support",
+      aliases: ["support"],
+      description: "Gets the support group links",
+      category: "general",
+      usage: `${client.config.prefix}Support`,
+      baseXp: 10,
+    });
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    await this.client.sendMessage(
+      M.sender.jid,
+      ` _*I'M FROM NEZUKO 🚀*\n\n
+        _*https://chat.whatsapp.com/BAtmmxd5rEhKH7Rx9gcZ4L`,
+
+      MessageType.text
+    );
+
+    return void M.reply("Sent you the Group Link in personal message");
+  };
+}
